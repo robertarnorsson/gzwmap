cb54227be240e82b27a800e5bc978d16da47a21e
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-12 10:30:23 +0200

    feat: Add briefing to "Leave No Man Behind" task

--- lib/data/tasks/leave-no-man-behind.ts
+++ lib/data/tasks/leave-no-man-behind.ts
@@ -35,7 +35,7 @@ const LeaveNoManBehind_Objective1_CSI: objective = {
 
 export const LeaveNoManBehind: task = {
   name: "Leave No Man Behind",
-  briefing: "",
+  briefing: "Since you proved yourself out there and know how to use our trackers, I have another job for you. We've lost a lot of men during our retreat, and many are still unaccounted for. We owe it to them and their loved ones to give them a proper farewell. I need you to find one of these poor bastards and mark his body so we can retrieve it. According to his squadmates, the soldier in question was last seen getting gunned down in one of the backyards around the town hall. I need you to search the area carefully, follow any signs, and mark the precise location with a GPS tracker, so make sure to bring one with you. Remember, we're professionals, and we honor our fallen with dignity. Even if ya'll think otherwise.",
   debriefing: "You've done the right thing. To be honest, I never liked the guy. He was too gung-ho and would always run his mouth, but that's not the point. We may have lost him in battle, but we haven't lost our sense of duty. Thanks to your efforts, his family will find solace in knowing their loved one will be returned to them, even if he was an asshole.",
   objectives: [
     LeaveNoManBehind_Objective1_LRI,
