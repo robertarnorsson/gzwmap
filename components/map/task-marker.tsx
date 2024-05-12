1d1de6766688d60cf38be42d6d34dd31d011e886
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-12 14:30:00 +0200

    Update rendering of task keys in TaskMarker component

--- components/map/task-marker.tsx
+++ components/map/task-marker.tsx
@@ -22,18 +22,26 @@ export default function TaskMarker({ map, task, objective }: { map: Map, task: t
         <p className="pt-4 text-sm text-primary text-pretty">{objective.name}</p>
         <p className="text-xs text-muted-foreground text-pretty">{objective.type}</p>
         <p className="pt-2 text-primary/85 text-pretty">{objective.description}</p>
-        {task.key && task.key.image && (
+        {task.keys && task.keys.length > 0 && (
             <div>
               <div className="flex flex-row gap-2 pt-2 mb-3">
                 <p className="text-xs text-muted-foreground">Requirements</p>
                 <p className="text-xs text-muted-foreground">▪</p>
                 <p className="text-xs text-muted-foreground">Keys</p>
               </div>
-              <div className="flex gap-1">
-                <Image className='item-key-image' width={50} height={50} src={task.key.image} alt={task.key.name} />
+              <div className="flex flex-wrap gap-1">
+                {task.keys.map((key, index) => (
+                    key.image && (
+                        <div key={index} className="flex gap-1">
+                          <Image className='item-key-image' width={50} height={50} src={key.image} alt={key.name} />
+                        </div>
+                    )
+                ))}
               </div>
             </div>
         )}
+
+
       </div>
   );
 
