e6e77733c8b489eb13c4f1e8d5f085ab2e9c5167
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-12 10:21:22 +0200

    style: Update task briefing for "Helping Hand" task

--- lib/data/tasks/helping-hand.ts
+++ lib/data/tasks/helping-hand.ts
@@ -34,7 +34,7 @@ const HelpingHand_Objective1_CSI: objective = {
 
 export const HelpingHand: task = {
   name: "Helping Hand",
-  briefing: "",
+  briefing: "We've got a problem. Seems our adversaries, those local rats, are way more well equipped than they ever should have been. Now, I know what you're thinking, \"How the fuck is that even possible?\" Well, it's clear as da that someone gave them a helping hand. And that is a problem we need to swiftly address. So here's the deal: we're gonna send you into one of their meeting spots, a place we suspect holds the answers we're looking for, to gather some concrete evidence, something that can expose who helped those bastards. Our scouts suggest that the best place to begin your search is an old restaurant on a street corner in the northern part of town. Once you've got what we need, make your way back here immediately. We don't want any loose ends that could come back to bite us in the ass.",
   debriefing: "Excellent work. I knew I could count on you to get the job done. You managed to discover a significant piece of evidence. It appears that those hicks struck some kind of deal with the local armed forces, the LAF. Now, we don't have all the answers just yet, but this discovery opens up new avenues for investigation. We'll analyze the folder and extract any actionable intelligence it contains.",
   objectives: [
     HelpingHand_Objective1_LRI,
