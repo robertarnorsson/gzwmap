d3904f150443e136add7b8bd4200fca113d89a94
Robert <rha006@gmail.com>
2024-05-12 23:29:01 +0200

    Removed bouncing at zoom limits

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -33,6 +33,7 @@ export default function Map() {
       doubleClickZoom={true}
       scrollWheelZoom={true}
       boxZoom={false}
+      bounceAtZoomLimits={false}
     >
       <TileLayer
         className="tile-map"
