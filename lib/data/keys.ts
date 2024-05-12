cb4cf43123c0f1d023e9fcc5946e29a9b5a53616
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-12 13:54:25 +0200

    Add new keys for various locations

---
+++ lib/data/keys.ts
@@ -0,0 +1,154 @@
+import { key } from "@/lib/types";
+import {BanPa, FortNarith, HuntersParadise, KiuVongsa, Sawmill, PhaLangAirfield, YBL1} from "@/lib/data/locations";
+
+export const BPWarehouse: key = {
+    name: "BP Warehouse",
+    image: "items/keys/bp-warehouse.png",
+    locations: [
+        BanPa
+    ]
+}
+
+export const FNDumpingGroundStorageShed: key = {
+    name: "FN Dumping Ground Storage Shed",
+    image: "items/keys/fn-dumping-ground-storage-shed.png",
+    locations: [
+        FortNarith
+    ]
+}
+
+export const HP101: key = {
+    name: "HP 101",
+    image: "items/keys/hp-101.png",
+    locations: [
+        HuntersParadise
+    ]
+}
+
+export const HP102: key = {
+    name: "HP 102",
+    image: "items/keys/hp-102.png",
+    locations: [
+        HuntersParadise
+    ]
+}
+
+export const HPWeaponStorage: key = {
+    name: "HP Weapon Storage",
+    image: "items/keys/hp-weapon-storage.png",
+    locations: [
+        HuntersParadise
+    ]
+}
+
+export const KVAttic: key = {
+    name: "KV Attic",
+    image: "items/keys/kv-attic.png",
+    locations: [
+        KiuVongsa
+    ]
+}
+
+export const KVDoctorsOffice: key = {
+    name: "KV Doctor's Office",
+    image: "items/keys/kv-doctors-office.png",
+    locations: [
+        KiuVongsa
+    ]
+}
+
+export const KVLumber: key = {
+    name: "KV Lumber",
+    image: "items/keys/kv-lumber.png",
+    locations: [
+        KiuVongsa
+    ]
+}
+
+export const KVMotel: key = {
+    name: "KV Motel",
+    image: "items/keys/kv-motel.png",
+    locations: [
+        KiuVongsa
+    ]
+}
+
+export const KVUnlra: key = {
+    name: "KV Unlra",
+    image: "items/keys/kv-unlra.png",
+    locations: [
+        KiuVongsa
+    ]
+}
+
+export const PLAStorageShed: key = {
+    name: "PLA Storage Shed",
+    image: "items/keys/pla-storage-shed.png",
+    locations: [
+        PhaLangAirfield
+    ]
+}
+
+export const SMOffice: key = {
+    name: "SM Office",
+    image: "items/keys/sm-office.png",
+    locations: [
+        Sawmill
+    ]
+}
+
+export const SMStorageShed: key = {
+    name: "SM Storage Shed",
+    image: "items/keys/sm-storage-shed.png",
+    locations: [
+        Sawmill
+    ]
+}
+
+export const YBL1CorridorDStorageRoom: key = {
+    name: "YBL 1 Corridor D Storage Room",
+    image: "items/keys/ybl-1-corridor-d-storage-room.png",
+    locations: [
+        YBL1
+    ]
+}
+
+export const YBL1GeneratorRoom: key = {
+    name: "YBL 1 Generator Room",
+    image: "items/keys/ybl-1-generator-room.png",
+    locations: [
+        YBL1
+    ]
+}
+
+export const YBL1MaintenanceDoor: key = {
+    name: "YBL 1 Maintenance Door",
+    image: "items/keys/ybl-1-maintenance-door.png",
+    locations: [
+        YBL1
+    ]
+}
+
+export const YBL1Office01: key = {
+    name: "YBL 1 Office 01",
+    image: "items/keys/ybl-1-office-01.png",
+    locations: [
+        YBL1
+    ]
+}
+
+export const YBL1Office02: key = {
+    name: "YBL 1 Office 02",
+    image: "items/keys/ybl-1-office-02.png",
+    locations: [
+        YBL1
+    ]
+}
+
+export const YBL1WeaponStorage: key = {
+    name: "YBL 1 Weapon Storage",
+    image: "items/keys/ybl-1-weapon-storage.png",
+    locations: [
+        YBL1
+    ]
+}
