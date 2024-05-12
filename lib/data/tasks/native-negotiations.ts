edfa21691cae626d1b0c27146f7efa44228af53f
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-12 10:42:46 +0200

    Add note about obtaining a briefcase from command tent

--- lib/data/tasks/native-negotiations.ts
+++ lib/data/tasks/native-negotiations.ts
@@ -61,6 +61,7 @@ export const NativeNegotiations: task = {
   name: "Native Negotiations",
   briefing: "Great news! We have a unique opportunity to enlist the support of a local native who wishes to join our cause. The thing is, they require payment upfront as a sign of trust, so I'll need you to deliver a case containing the agreed-upon amount to a designated drop-off point, which you'll find below a truck in the town's car repair shop. Once you reach the location, just leave the case with the money and return back. Please don't engage in unecessary interactions or reveal our intentions to anyone else. This transaction must remain discreet and confidential. I don't want to put anybody at risk more than we already have. The native's knowledge of the region and local dynamics could significantly benefit our operations, so deliver the payment and return safely!",
   debriefing: "Excellent work! The potential benefits of this alliance cannot be overstated. Our access to local knowledge and resources will greatly enhance our capabilities in this challenging environment. We have taken a significant step forward in securing a strong foothold in this region.",
+  note: "You need a briefcase from the command tent in the base camp of the faction you are working for.",
   objectives: [
     NativeNegotiations_Objective1_LRI,
     NativeNegotiations_Objective2_LRI,
