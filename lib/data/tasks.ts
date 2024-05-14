9e9b968ea25530659cda39bdb28abed0c7298e80
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-14 18:19:34 +0200

    Add new recon mission task "I went, I saw, I conquered - Part 1".

--- lib/data/tasks.ts
+++ lib/data/tasks.ts
@@ -1,51 +1,52 @@
 import { task } from "@/lib/types";
 
-import { FirstRecon }              from "@/lib/data/tasks/first-recon";
-import { CacheRetrieval }          from "@/lib/data/tasks/cache-retrieval";
-import { MedsWanted }              from "@/lib/data/tasks/meds-wanted";
-import { LeaveNoManBehind }        from "@/lib/data/tasks/leave-no-man-behind";
-import { LittleBirdDown }          from "@/lib/data/tasks/little-bird-down";
-import { RestoringOrder }          from "@/lib/data/tasks/restoring-order";
-import { RatsNest }                from "@/lib/data/tasks/rats-nest";
-import { LostAndFound }            from "@/lib/data/tasks/lost-and-found";
-import { MedicalDetective }        from "@/lib/data/tasks/medical-detective";
-import { HelpingHand }             from "@/lib/data/tasks/helping-hand";
-import { SecretCompassion }        from "@/lib/data/tasks/secret-compassion";
-import { RadioSilence }            from "@/lib/data/tasks/radio-silence";
-import { Reclamation }             from "@/lib/data/tasks/reclamation";
-import { FirstHit }                from "@/lib/data/tasks/first-hit";
-import { NativeNegotiations }      from "@/lib/data/tasks/native-negotiations";
-import { CovertResearch }          from "@/lib/data/tasks/covert-research";
-import { ToolsOfTheTrade }         from "@/lib/data/tasks/tools-of-the-trade";
-import { LastFarewell }            from "@/lib/data/tasks/last-farewell";
-import { LeftBehind }              from "@/lib/data/tasks/left-behind";
-import { LastFlight }              from "@/lib/data/tasks/last-flight";
-import { ASingleDrop }             from "@/lib/data/tasks/a-single-drop";
-import { SkycrawlersPartOne }      from "@/lib/data/tasks/skycrawlers-part-1";
-import { SkycrawlersPartTwo }      from "@/lib/data/tasks/skycrawlers-part-2";
-import { WingClipping }            from "@/lib/data/tasks/wing-clipping";
-import { BusinessOrPleasure }      from "@/lib/data/tasks/business-or-pleasure";
-import { FinalCheckout }           from "@/lib/data/tasks/final-checkout"
-import { ManifestDestiny }         from "@/lib/data/tasks/manifest-destiny"
-import { AtTheMoundsOfMadness }    from "@/lib/data/tasks/at-the-mounds-of-madness"
-import { ItsInTheWaterPartOne }    from "@/lib/data/tasks/its-in-the-water-part-1"
-import { ItsInTheWaterPartTwo }    from "@/lib/data/tasks/its-in-the-water-part-2"
-import {TheShadowOverBanPaPartOne} from "@/lib/data/tasks/the-shadow-over-ban-pa-part-1"
-import {TheShadowOverBanPaPartTwo} from "@/lib/data/tasks/the-shadow-over-ban-pa-part-2"
-import { LineInTheSand }           from "@/lib/data/tasks/line-in-the-sand"
-import { TheValueOfSharing }       from "@/lib/data/tasks/the-value-of-sharing"
-import { UninvitedGuests }         from "@/lib/data/tasks/uninvited-guests"
-import { WarmWelcome }             from "@/lib/data/tasks/warm-welcome"
-import { LamangConnection }        from "@/lib/data/tasks/lamang-connection"
-import { TheMostDangerousGame }    from "@/lib/data/tasks/the-most-dangerous-game";
-import { OnTheRange }              from "@/lib/data/tasks/on-the-range";
-import { GunsAndAmmo }             from "@/lib/data/tasks/guns-and-ammo";
-import { ShootingGallery }         from "@/lib/data/tasks/shooting-gallery";
-import { TrueGrit }                from "@/lib/data/tasks/true-grit";
-import { OneShotOneKill }          from "@/lib/data/tasks/one-shot-one-kill";
-import { TheBrave }                from "@/lib/data/tasks/the-brave";
-import { Carnivore }               from "@/lib/data/tasks/carnivore";
-import { UpToSnuff }               from "@/lib/data/tasks/up-to-snuff";
+import { FirstRecon }                     from "@/lib/data/tasks/first-recon";
+import { CacheRetrieval }                 from "@/lib/data/tasks/cache-retrieval";
+import { MedsWanted }                     from "@/lib/data/tasks/meds-wanted";
+import { LeaveNoManBehind }               from "@/lib/data/tasks/leave-no-man-behind";
+import { LittleBirdDown }                 from "@/lib/data/tasks/little-bird-down";
+import { RestoringOrder }                 from "@/lib/data/tasks/restoring-order";
+import { RatsNest }                       from "@/lib/data/tasks/rats-nest";
+import { LostAndFound }                   from "@/lib/data/tasks/lost-and-found";
+import { MedicalDetective }               from "@/lib/data/tasks/medical-detective";
+import { HelpingHand }                    from "@/lib/data/tasks/helping-hand";
+import { SecretCompassion }               from "@/lib/data/tasks/secret-compassion";
+import { RadioSilence }                   from "@/lib/data/tasks/radio-silence";
+import { Reclamation }                    from "@/lib/data/tasks/reclamation";
+import { FirstHit }                       from "@/lib/data/tasks/first-hit";
+import { NativeNegotiations }             from "@/lib/data/tasks/native-negotiations";
+import { CovertResearch }                 from "@/lib/data/tasks/covert-research";
+import { ToolsOfTheTrade }                from "@/lib/data/tasks/tools-of-the-trade";
+import { LastFarewell }                   from "@/lib/data/tasks/last-farewell";
+import { LeftBehind }                     from "@/lib/data/tasks/left-behind";
+import { LastFlight }                     from "@/lib/data/tasks/last-flight";
+import { ASingleDrop }                    from "@/lib/data/tasks/a-single-drop";
+import { SkycrawlersPartOne }             from "@/lib/data/tasks/skycrawlers-part-1";
+import { SkycrawlersPartTwo }             from "@/lib/data/tasks/skycrawlers-part-2";
+import { WingClipping }                   from "@/lib/data/tasks/wing-clipping";
+import { BusinessOrPleasure }             from "@/lib/data/tasks/business-or-pleasure";
+import { FinalCheckout }                  from "@/lib/data/tasks/final-checkout"
+import { ManifestDestiny }                from "@/lib/data/tasks/manifest-destiny"
+import { AtTheMoundsOfMadness }           from "@/lib/data/tasks/at-the-mounds-of-madness"
+import { ItsInTheWaterPartOne }           from "@/lib/data/tasks/its-in-the-water-part-1"
+import { ItsInTheWaterPartTwo }           from "@/lib/data/tasks/its-in-the-water-part-2"
+import {TheShadowOverBanPaPartOne}        from "@/lib/data/tasks/the-shadow-over-ban-pa-part-1"
+import {TheShadowOverBanPaPartTwo}        from "@/lib/data/tasks/the-shadow-over-ban-pa-part-2"
+import { LineInTheSand }                  from "@/lib/data/tasks/line-in-the-sand"
+import { TheValueOfSharing }              from "@/lib/data/tasks/the-value-of-sharing"
+import { UninvitedGuests }                from "@/lib/data/tasks/uninvited-guests"
+import { WarmWelcome }                    from "@/lib/data/tasks/warm-welcome"
+import { LamangConnection }               from "@/lib/data/tasks/lamang-connection"
+import { TheMostDangerousGame }           from "@/lib/data/tasks/the-most-dangerous-game";
+import { OnTheRange }                     from "@/lib/data/tasks/on-the-range";
+import { GunsAndAmmo }                    from "@/lib/data/tasks/guns-and-ammo";
+import { ShootingGallery }                from "@/lib/data/tasks/shooting-gallery";
+import { TrueGrit }                       from "@/lib/data/tasks/true-grit";
+import { OneShotOneKill }                 from "@/lib/data/tasks/one-shot-one-kill";
+import { TheBrave }                       from "@/lib/data/tasks/the-brave";
+import { Carnivore }                      from "@/lib/data/tasks/carnivore";
+import { UpToSnuff }                      from "@/lib/data/tasks/up-to-snuff";
+import { IWentISawIConqueredPartOne }     from "@/lib/data/tasks/i-went-i-saw-i-conquered-part-1";
 
 export const Tasks: task[] = [
   FirstRecon,
@@ -93,5 +94,6 @@ export const Tasks: task[] = [
   OneShotOneKill,
   TheBrave,
   Carnivore,
-  UpToSnuff
+  UpToSnuff,
+  IWentISawIConqueredPartOne
 ]
