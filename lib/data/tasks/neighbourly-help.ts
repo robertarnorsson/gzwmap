532cf6fea60edde011ce9328647d83338f366901
Dennis Elsinga <personal.elsinga@gmail.com>
2024-05-16 20:28:04 +0200

    Investigate military base fuel supply

--- lib/data/tasks/neighbourly-help.ts
+++ lib/data/tasks/neighbourly-help.ts
@@ -19,8 +19,8 @@ const NeighbourlyHelp_Objective2: objective = {
 
 export const NeighbourlyHelp: task = {
     name: "Neighbourly Help",
-    briefing: "Hey there, buddy. I've got a special task for you. You're gonna be a cameraman of sorts. I need you to infiltrate the military base and hack into their cameras. We need as much information on them as possible. I don't want to go a minute without being alerted when soldiers go to the HQ or leave the base. I want them on my screen 24-7. Like some Big Brother bullshit. Take these gadgets and place them in the cameras on the base. Soon, we'll know more about them than their mothers.",
-    debriefing: "Wonderful! With these taped cameras, we now have the upper hand in monitoring the military base. I feel like I'm in a spy movie or something. No one will pick their nose without our knowledge. You did well, champ.",
+    briefing: "Our situation is not getting any better. We are still having problems getting almost everything we need from the mainland. Especially fuel would be super useful right now. Fortunately, the military base seems to be well supplied. We know they've got various vehicles and other machines that eat gas like Kentuckians eat fast food. I need you to go to the military base and see how their fuel supply is doing. Every drop that we can take from them is a big plus for us. We have to scavenge as much as possible. Out here, each day is an uphill struggle. It's like some goddamn post-apocalyptic movie.",
+    debriefing: "No fuel at all? So they have all that stuff just sitting around? Damn it. On the one hand it's nice that they can't use the vehicles against us, but on the other hand it means that we have to look for the juice somewhere else. If it wasn't for that cursed blockade...",
     note: "Both fuel gauges are located in the same area.",
     objectives: [
         NeighbourlyHelp_Objective1,
