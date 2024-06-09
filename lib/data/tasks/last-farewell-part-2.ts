177aec2901193f529f6afa2f89e55c38fffc2330
Robert <rha006@gmail.com>
2024-06-10 01:05:09 +0200

    Added around half of MS tasks

---
+++ lib/data/tasks/last-farewell-part-2.ts
@@ -0,0 +1,25 @@
+import { MarkerType, ObjectiveType, objective, task } from "@/lib/types";
+import { Gunny } from "@/lib/data/vendors";
+import { MidnightSapphire } from "@/lib/data/locations";
+
+const LastFarewellPartTwo_Objective1: objective = {
+    id: "",
+    name: "Body",
+    description: "Find the body laying in the grass near the flag",
+    position: [15979.076629630958, 21996.82500708069],
+    type: ObjectiveType.LOCATE,
+    types: [MarkerType.TASK, MarkerType.LOCATE, MarkerType.GUNNY, MarkerType.MIDNIGHTSAPPHIRE],
+    location: MidnightSapphire
+}
+
+export const LastFarewellPartTwo: task = {
+    id: "",
+    name: "Last Farewell - Part 2",
+    briefing: "I have yet another fallen colleague: Jason Novak. He fell out of a helicopter during a flyby of Madnight Sapphire. Damn shame we lost him just because he didn't follow security protocols. Dammit, what do I always say? Don't do anything stupid and don't get killed! And he just HAD to do both, didn't he? Sometimes I really think you are all just a bunch of overgrown children running around with guns... I want you to search the Midnight Sapphire Resort area, find his body, and mark it, just like before. The accident happened somewhere around the golf course, so I suggest you go there.",
+    debriefing: "That poor stupid bastard. He was one of those that would alwas crack jokes, you know? Never any good ones, though. Guess his final joke was one him. After all, he hit a hole in one.",
+    objectives: [
+      LastFarewellPartTwo_Objective1
+    ],
+    vendor: Gunny,
+    level: 0,
+}
---
+++ lib/data/tasks/nobel-powder.ts
@@ -0,0 +1,25 @@
+import { MarkerType, ObjectiveType, objective, task } from "@/lib/types";
+import { Banshee } from "@/lib/data/vendors";
+import { MidnightSapphire } from "@/lib/data/locations";
+
+const NobelPowder_Objective1: objective = {
+    id: "",
+    name: "Evidence of drug trade",
+    description: "Retrieve evidence of drug trade in Midnight Sapphire and report back to Banshee",
+    position: [16538.73054286704, 21616.076155810195],
+    type: ObjectiveType.COLLECT,
+    types: [MarkerType.TASK, MarkerType.COLLECT, MarkerType.BANSHEE, MarkerType.MIDNIGHTSAPPHIRE],
+    location: MidnightSapphire
+}
+
+export const NobelPowder: task = {
+    id: "",
+    name: "Nobel Powder",
+    briefing: "Do you have a moment? I am sure you have noticed that the island is a paradise for drugs; they are on every corner. We suspect that the government has been controlling the drug trade from the shadows. That's why we need as much proof as possible. The Midnight Sapphire was a resort for the wealthiest people, so I'm 100% sure there would be a specific place where they would keep it close enough to the guests. The rich are drawn to drugs like moths to a flame. Search the Midnight Sapphire Hotel and bring me some hard evidence.",
+    debriefing: "So, the reception took care of the sales, and the garbage room was redecorated to suit a drug stash, a fitting place. I appreciate your help. Thanks to these findings, we are closer to our goal. I can't wait to put the handcuffs on the gangsters running this country.",
+    objectives: [
+      NobelPowder_Objective1
+    ],
+    vendor: Banshee,
+    level: 0,
+}
