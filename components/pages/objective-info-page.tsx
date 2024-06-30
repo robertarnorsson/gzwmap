92839c9bd1a8d21b41adffc71f7c786ecec1087c
Robert <rha006@gmail.com>
2024-06-30 18:55:32 +0200

    Cleaned up task detail page and removed objective detail page

--- components/pages/objective-info-page.tsx
+++
@@ -1,9 +0,0 @@
-import { objective } from "@/lib/types";
-
-export default function ObjectiveInfoPage({ objective }: {objective: objective}) {
-  return (
-    <div>
-      {objective.name}
-    </div>
-  );
-};
--- components/pages/task-info-page.tsx
+++ components/pages/task-info-page.tsx
@@ -4,9 +4,6 @@ import { task, objective } from "@/lib/types";
 import Image from "next/image";
 import { useMemo } from "react";
 
-import SimpleBar from 'simplebar-react';
-import 'simplebar-react/dist/simplebar.min.css';
-
 import {
   Accordion,
   AccordionContent,
@@ -14,6 +11,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Item } from "../comp/item";
+import { toSlug } from "@/lib/utils";
 
 export default function TaskInfoPage({ task }: { task: task }) {
   const groupedObjectives = useMemo(() => {
@@ -28,61 +26,24 @@ export default function TaskInfoPage({ task }: { task: task }) {
     return groups;
   }, [task.objectives]);
 
-  const allSameLocation = useMemo(() => {
-    const locations = new Set(task.objectives.map((obj) => obj.location.name));
-    return locations.size === 1;
-  }, [task.objectives]);
-
   return (
     <div className="container mx-auto p-6 bg-background text-foreground flex">
-      <div className="flex-1 bg-card shadow-lg rounded-lg p-6 mb-6">
-        <div className="flex flex-col h-full">
-          <h1 className="text-4xl font-bold mb-4">{task.name}</h1>
-          <div className="grid grid-cols-1 gap-6 flex-1 overflow-y-auto">
-            <div className="col-span-2">
-              {allSameLocation ? (
-                <div>
-                  <h2 className="text-2xl font-semibold mb-2">
-                    {task.objectives[0].location.name}
-                  </h2>
-                  <ul className="mb-4 border border-muted rounded-lg">
-                    {task.objectives.map((objective, index) => (
-                      <li
-                        key={objective.id}
-                        className="p-4 border-b last:border-none border-muted"
-                      >
-                        <strong className="block">{`Stage ${index + 1}`}</strong>
-                        <p>{objective.description}</p>
-                        {objective.location && (
-                          <p>
-                            <strong>Location:</strong> {objective.location.name}
-                          </p>
-                        )}
-                        {objective.faction && (
-                          <p>
-                            <strong>Faction:</strong> {objective.faction.name}
-                          </p>
-                        )}
-                        {objective.image && (
-                          <div className="relative mt-2 max-w-xs rounded-lg overflow-hidden">
-                            <Image
-                              src={objective.image}
-                              alt={objective.name}
-                              layout="responsive"
-                              width={300}
-                              height={200}
-                              objectFit="cover"
-                              className="rounded-lg"
-                            />
-                          </div>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : (
-                <Accordion type="single" collapsible>
-                  {Object.entries(groupedObjectives).map(([locationName, objectives]) => (
+      <div className="flex-1">
+        <div className="flex-1 bg-card shadow-lg rounded-lg p-6 mb-6">
+          <div className="flex flex-col h-full">
+            <h1 className="text-4xl font-bold mb-4">{task.name}</h1>
+            <div className="flex flex-row gap-2">
+              <p className="text-sm text-muted-foreground">{task.vendor.name}</p>
+              <p className="text-sm text-muted-foreground">▪</p>
+              <p className="text-sm text-muted-foreground">{task.objectives[0].type}</p>
+              <p className="text-sm text-muted-foreground">▪</p>
+              <p className="text-sm text-muted-foreground">{task.objectives[0].location?.name}</p>
+            </div>
+            <div className="grid grid-cols-1 gap-6 flex-1 overflow-y-auto">
+              <div className="col-span-2">
+                <h2 className="text-2xl font-bold mt-4">Objectives</h2>
+                <Accordion type="single" collapsible className="overflow-x-hidden" defaultValue={task.objectives.at(0)?.location.name}>
+                  {Object.entries(groupedObjectives).map(([locationName, objectives], index) => (
                     <AccordionItem key={locationName} value={locationName}>
                       <AccordionTrigger>
                         <h2 className="text-2xl font-semibold mb-2">{locationName}</h2>
@@ -94,7 +55,8 @@ export default function TaskInfoPage({ task }: { task: task }) {
                               key={index}
                               className="p-4 border-b last:border-none border-muted"
                             >
-                              <strong className="block">{`Stage ${index + 1}`}</strong>
+                              <p className="block text-xs font-light text-muted-foreground">{`Stage ${index + 1}`}</p>
+                              <h2 id={toSlug(objective.name)} className="text-xl font-semibold mb-2">{objective.name}</h2>
                               <p>{objective.description}</p>
                               {objective.location && (
                                 <p>
@@ -111,11 +73,9 @@ export default function TaskInfoPage({ task }: { task: task }) {
                                   <Image
                                     src={objective.image}
                                     alt={objective.name}
-                                    layout="responsive"
-                                    width={300}
+                                    className="w-full h-full aspect-video rounded-lg object-cover"
                                     height={200}
-                                    objectFit="cover"
-                                    className="rounded-lg"
+                                    width={300}
                                   />
                                 </div>
                               )}
@@ -124,28 +84,65 @@ export default function TaskInfoPage({ task }: { task: task }) {
                         </ul>
                       </AccordionContent>
                     </AccordionItem>
-                  ))
-                }
+                  ))}
                 </Accordion>
-              )}
 
-              <div className="col-span-1">
-                <Accordion type="single" collapsible>
-                  <AccordionItem value="briefing">
-                    <AccordionTrigger>
-                      <h2 className="text-2xl font-semibold mb-2">Briefing</h2>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      {task.briefing && (
-                        <div className="mb-4">
-                          <p>{task.briefing}</p>
-                        </div>
-                      )}
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
+                <div className="col-span-1">
+                  <Accordion type="multiple" className="overflow-x-hidden">
+                    <AccordionItem value="briefing">
+                      <AccordionTrigger>
+                        <h2 className="text-2xl font-semibold mb-2">Briefing</h2>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        {task.briefing && (
+                          <div className="mb-4">
+                            <p>{task.briefing}</p>
+                          </div>
+                        )}
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="debriefing">
+                      <AccordionTrigger>
+                        <h2 className="text-2xl font-semibold mb-2">Debriefing</h2>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        {task.debriefing && (
+                          <div className="mb-4">
+                            <p>{task.debriefing}</p>
+                          </div>
+                        )}
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div className="md:w-1/3 flex flex-col ml-6">
+        <div className="sticky top-6 rounded-lg">
+          <div className="relative">
+              <Image 
+                src={`/task_banners/${task.vendor.name.replaceAll(" ", "").toLocaleLowerCase()}_task_banner.webp`}
+                alt={task.name}
+                className="w-full h-auto object-cover rounded-t-lg"
+                height={100}
+                width={400}
+                priority
+              />
+              <div className="absolute top-1/2 left-0 right-0 bottom-0 bg-gradient-to-t from-card"></div>
+            </div>
+            <div className="bg-card p-4 rounded-lg">
+              <h2 className="text-2xl font-semibold mb-2">{task.name}</h2>
+              <div className="flex flex-row gap-2">
+                <p className="text-sm text-muted-foreground">{task.objectives[0].type}</p>
+                <p className="text-sm text-muted-foreground">▪</p>
+                <p className="text-sm text-muted-foreground">{task.vendor.name}</p>
+                <p className="text-sm text-muted-foreground">▪</p>
+                <p className="text-sm text-muted-foreground">{task.objectives[0].location?.name}</p>
+              </div>
               {task.items && task.items.length > 0 && (
                 <div>
                   <h2 className="text-2xl font-semibold mb-2">Items</h2>
@@ -166,29 +163,8 @@ export default function TaskInfoPage({ task }: { task: task }) {
               )}
             </div>
           </div>
-        </div>
       </div>
-
-      <div className="md:w-1/3 flex flex-col ml-6">
-        <div className="sticky top-6 bg-card p-4 rounded-lg">
-          <h2 className="text-2xl font-semibold mb-2">{task.name}</h2>
-          <p>
-            <strong>Type:</strong> {task.objectives[0].type}
-          </p>
-          <p>
-            <strong>Given By:</strong> {task.vendor.name}
-          </p>
-          <p>
-            <strong>Location:</strong> {task.objectives[0].location?.name}
-          </p>
-          {/* <h3 className="text-xl font-semibold mt-4">Rewards</h3>
-          <ul>
-            <li>2x AKMN</li>
-            <li>1000 Experience</li>
-            <li>150 Reputation</li>
-          </ul> */}
-        </div>
-      </div>
+        
     </div>
   );
 }
