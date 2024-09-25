b28d102ffaea6e822996c342e419349388df5b9d
Robert <rha006@gmail.com>
2024-09-25 23:16:56 +0200

    Made task info page more mobile friendly

--- components/pages/task-info-page.tsx
+++ components/pages/task-info-page.tsx
@@ -3,7 +3,6 @@
 import { task, objective } from "@/lib/types";
 import Image from "next/image";
 import { useMemo } from "react";
-
 import {
   Accordion,
   AccordionContent,
@@ -28,14 +27,72 @@ export default function TaskInfoPage({ task }: { task: task }) {
   }, [task.objectives]);
 
   return (
-    <div className="container mx-auto p-6 bg-background text-foreground flex">
+    <div className="container mx-auto p-4 bg-background text-foreground flex flex-col md:flex-row">
+      <div className="w-full md:w-1/3 flex flex-col mb-6 md:mb-0 md:ml-6 order-first md:order-last">
+        <div className="sticky top-4 rounded-lg">
+          <div className="relative">
+            <Image
+              src={`/task_banners/${task.vendor.name.replaceAll(" ", "").toLowerCase()}_task_banner.webp`}
+              alt={task.name}
+              className="w-full h-auto object-cover rounded-t-lg"
+              height={100}
+              width={400}
+              priority
+            />
+            <div className="absolute top-1/2 left-0 right-0 bottom-0 bg-gradient-to-t from-card"></div>
+          </div>
+          <div className="bg-card p-4 rounded-lg">
+            <h2 className="text-3xl md:text-2xl font-semibold mb-2">{task.name}</h2>
+            <p className="text-sm text-muted-foreground">{task.vendor.name}</p>
+            {task.items ? task.items.length > 0 && (
+              <div>
+                <h2 className="text-lg md:text-2xl font-semibold mt-3 mb-2">Items</h2>
+                <div className="flex flex-wrap gap-2">
+                  {task.items.map((item, index) => (
+                    <Item
+                      key={index}
+                      name={item.name}
+                      shortName={item.shortName}
+                      size={item.size}
+                      imageUrl={item.image}
+                    />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div className="flex h-24 justify-center items-center">
+                <p className="text-muted-foreground">No items needed</p>
+              </div>
+            )}
+            {task.key && task.key.length > 0 && (
+              <div>
+                <h2 className="text-lg md:text-2xl font-semibold mt-3 mb-2">
+                  {task.key.length === 1 ? "Key" : "Keys"}
+                </h2>
+                <div className="flex flex-wrap gap-2">
+                  {task.key.map((key, index) => (
+                    <Item
+                      key={index}
+                      name={key.name}
+                      shortName={key.shortName}
+                      size={key.size}
+                      imageUrl={key.image}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
       <div className="flex-1">
-        <div className="flex-1 bg-card shadow-lg rounded-lg p-6 mb-6">
+        <div className="bg-card shadow-lg rounded-lg p-4 mb-6">
           <div className="flex flex-col h-full">
-            <h1 className="text-4xl font-bold mb-4">{task.name}</h1>
-            <div className="grid grid-cols-1 gap-6 flex-1 overflow-y-auto">
-              <div className="col-span-2">
-                <h1 className="text-3xl font-bold mt-4">Objectives</h1>
+            <h1 className="text-2xl md:text-4xl font-bold mb-4">{task.name}</h1>
+            <div className="grid grid-cols-1 gap-4 md:gap-6 flex-1 overflow-y-auto">
+              <div>
+                <h1 className="text-xl md:text-3xl font-bold mt-4">Objectives</h1>
                 <Accordion
                   type="single"
                   collapsible
@@ -46,7 +103,7 @@ export default function TaskInfoPage({ task }: { task: task }) {
                     ([locationName, objectives], index) => (
                       <AccordionItem key={index} value={locationName}>
                         <AccordionTrigger>
-                          <h2 className="text-2xl font-semibold">
+                          <h2 className="text-lg md:text-2xl font-semibold">
                             {locationName}
                           </h2>
                         </AccordionTrigger>
@@ -55,67 +112,56 @@ export default function TaskInfoPage({ task }: { task: task }) {
                             {objectives.map((objective, index) => (
                               <li
                                 key={index}
-                                className="p-4 border-b last:border-none flex flex-row items-start"
+                                className="p-2 md:p-4 border-b last:border-none flex flex-col md:flex-row items-start"
                               >
-                                <div className="flex flex-1 flex-col justify-around">
+                                <div className="flex-1 flex flex-col">
                                   <div>
                                     <p className="block text-xs font-light text-muted-foreground">
                                       {`Stage ${index + 1}`}
                                     </p>
                                     <h2
                                       id={toSlug(objective.name)}
-                                      className="text-xl font-semibold"
+                                      className="text-lg md:text-xl font-semibold"
                                     >
                                       {objective.name}
                                     </h2>
-                                    <div className="flex flex-row gap-2">
-                                      <p className="text-xs text-muted-foreground">
-                                        {objective.type}
-                                      </p>
-                                      <p className="text-xs text-muted-foreground">▪</p>
-                                      <p className="text-xs text-muted-foreground">
-                                        {task.vendor.name}
-                                      </p>
-                                      <p className="text-xs text-muted-foreground">▪</p>
-                                      <p className="text-xs text-muted-foreground">
-                                        {objective.location?.name}
-                                      </p>
+                                    <div className="flex flex-wrap gap-2 text-xs text-muted-foreground">
+                                      <p>{objective.type}</p>
+                                      <p>▪</p>
+                                      <p>{task.vendor.name}</p>
+                                      <p>▪</p>
+                                      <p>{objective.location?.name}</p>
                                     </div>
                                     <p>{objective.description}</p>
                                   </div>
-                                  <div>
+                                  <div className="mt-2">
                                     {objective.items && (
-                                      <div className="flex flex-wrap mt-2 gap-1">
-                                        <div className="flex gap-1">
-                                          {objective.items.map((item, index) => (
-                                            <Item
-                                              key={index}
-                                              name={item.name}
-                                              shortName={item.shortName}
-                                              size={item.size}
-                                              imageUrl={item.image}
-                                            />
-                                          ))}
-                                        </div>
+                                      <div className="flex flex-wrap gap-2">
+                                        {objective.items.map((item, index) => (
+                                          <Item
+                                            key={index}
+                                            name={item.name}
+                                            shortName={item.shortName}
+                                            size={item.size}
+                                            imageUrl={item.image}
+                                          />
+                                        ))}
                                       </div>
                                     )}
                                     {objective.key && (
-                                      <div className="flex flex-wrap mt-2 gap-1">
-                                        <div className="flex gap-1">
-                                          <Item
-                                              key={index}
-                                              name={objective.key.name}
-                                              shortName={objective.key.shortName}
-                                              size={objective.key.size}
-                                              imageUrl={objective.key.image}
-                                            />
-                                        </div>
+                                      <div className="flex flex-wrap gap-2">
+                                        <Item
+                                          name={objective.key.name}
+                                          shortName={objective.key.shortName}
+                                          size={objective.key.size}
+                                          imageUrl={objective.key.image}
+                                        />
                                       </div>
                                     )}
                                   </div>
                                 </div>
                                 {objective.image && (
-                                  <div className="relative h-[180px] ml-4 max-w-xs rounded-lg overflow-hidden">
+                                  <div className="relative h-[150px] md:h-[180px] mt-4 md:mt-0 ml-0 md:ml-4 w-full md:max-w-xs rounded-lg overflow-hidden">
                                     <ImageShowcase
                                       url={objective.image}
                                       alt={objective.name}
@@ -131,18 +177,16 @@ export default function TaskInfoPage({ task }: { task: task }) {
                   )}
                 </Accordion>
 
-                <div className="col-span-1">
-                  <h1 className="text-3xl font-bold mt-16">Information</h1>
-                  <Accordion type="multiple" className="overflow-x-hidden">
+                <div className="mt-8">
+                  <h1 className="text-xl md:text-3xl font-bold">Information</h1>
+                  <Accordion type="multiple" className="overflow-x-hidden mt-4">
                     <AccordionItem value="briefing">
                       <AccordionTrigger>
-                        <h2 className="text-2xl font-semibold">
-                          Briefing
-                        </h2>
+                        <h2 className="text-lg md:text-2xl font-semibold">Briefing</h2>
                       </AccordionTrigger>
                       <AccordionContent>
                         {task.briefing && (
-                          <div className="p-4">
+                          <div className="p-2 md:p-4">
                             <p>{task.briefing}</p>
                           </div>
                         )}
@@ -150,13 +194,11 @@ export default function TaskInfoPage({ task }: { task: task }) {
                     </AccordionItem>
                     <AccordionItem value="debriefing">
                       <AccordionTrigger>
-                        <h2 className="text-2xl font-semibold">
-                          Debriefing
-                        </h2>
+                        <h2 className="text-lg md:text-2xl font-semibold">Debriefing</h2>
                       </AccordionTrigger>
                       <AccordionContent>
                         {task.debriefing && (
-                          <div className="p-4">
+                          <div className="p-2 md:p-4">
                             <p>{task.debriefing}</p>
                           </div>
                         )}
@@ -166,70 +208,6 @@ export default function TaskInfoPage({ task }: { task: task }) {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="md:w-1/3 flex flex-col ml-6">
-        <div className="sticky top-6 rounded-lg">
-          <div className="relative">
-            <Image
-              src={`/task_banners/${task.vendor.name
-                .replaceAll(" ", "")
-                .toLocaleLowerCase()}_task_banner.webp`}
-              alt={task.name}
-              className="w-full h-auto object-cover rounded-t-lg"
-              height={100}
-              width={400}
-              priority
-            />
-            <div className="absolute top-1/2 left-0 right-0 bottom-0 bg-gradient-to-t from-card"></div>
-          </div>
-          <div className="bg-card p-4 rounded-lg">
-            <h2 className="text-2xl font-semibold mb-2">{task.name}</h2>
-            <p className="text-sm text-muted-foreground">
-              {task.vendor.name}
-            </p>
-            {task.items ? task.items.length > 0 && (
-              <div>
-                <h2 className="text-2xl font-semibold mt-3 mb-2">Items</h2>
-                <div className="flex flex-wrap gap-1">
-                  <div className="flex gap-1">
-                    {task.items.map((item, index) => (
-                      <Item
-                        key={index}
-                        name={item.name}
-                        shortName={item.shortName}
-                        size={item.size}
-                        imageUrl={item.image}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="flex h-24 justify-center items-center">
-                <p className="text-muted-foreground">No items needed</p>
-              </div>
-            )}
-            {task.key && task.key.length > 0 && (
-              <div>
-                <h2 className="text-2xl font-semibold mt-3 mb-2">{task.key.length == 1 ? "Key" : "Keys"}</h2>
-                <div className="flex flex-wrap gap-1">
-                  <div className="flex gap-1">
-                    {task.key.map((key, index) => (
-                      <Item
-                        key={index}
-                        name={key.name}
-                        shortName={key.shortName}
-                        size={key.size}
-                        imageUrl={key.image}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
-            )}
           </div>
         </div>
       </div>
