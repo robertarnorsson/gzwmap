e33f2b411c4eb6a93836705dfb7bb25c606b4605
robertarnorsson <rha006@gmail.com>
2024-11-26 19:38:07 +0100

    Updated site meta tags

--- app/routes/donate.tsx
+++
@@ -1,5 +0,0 @@
-import { LoaderFunction, redirect } from "@remix-run/cloudflare";
-
-export const loader: LoaderFunction = async () => {
-  return redirect("https://www.paypal.com/donate/?hosted_button_id=MG4RA9V5VULLG");
-};
